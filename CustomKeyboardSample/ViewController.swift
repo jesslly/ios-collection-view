0ced16a1e0804a3284a664f0ee41c82b9d0bac12
Jesslly Wong <jesslly.wong@netsuite.com>
2016-04-20 15:43:25 -0700

    Initial Commit

---
+++ CustomKeyboardSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomKeyboardSample
+//
+//  Created by Wong, Jesslly on 2016-04-20.
+//  Copyright © 2016 Wong, Jesslly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
